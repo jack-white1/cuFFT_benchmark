c75316a8c31ed194e7d3fef4c74fe77b3c16d57d
Jack White <jackajwhite@gmail.com>
2020-11-09 15:50:37 +0000

    python test

--- cuFFT.py
+++ cuFFT.py
@@ -2,5 +2,5 @@ import os
 import subprocess
 FNULL = open(os.devnull, 'w')
 config =  "1024 0 0 1 10 f C2C 0"
-args = "cuFFT_benchmark.exe " + config
+args = "cuFFT_benchmark.exe"# + config
 subprocess.call(args, stdout=FNULL, stderr=FNULL, shell=False)
