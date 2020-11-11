a5098a064619139097410b511556442174ecf5f1
Jack White <jackajwhite@gmail.com>
2020-11-11 14:39:28 +0000

    bringing remote up to date

--- cuFFT.py
+++ cuFFT.py
@@ -1,6 +1,18 @@
 import os
 import subprocess
+import matplotlib.pyplot as plt
+
 FNULL = open(os.devnull, 'w')
-config =  " 1024 0 0 1 10 f C2C 0"
+
+Nx = "1024"
+Ny = "0"
+Nz = "0"
+N_FFTs = "1"
+N_runs = "1"
+precision = "b"
+FFT_type = "C2C"
+device_id = "0"
+config = " "+Nx+" "+Ny+" "+Nz+" "+N_FFTs+" "+N_runs+" "+precision+" "+FFT_type+" "+device_id
+
 args = "cuFFT_benchmark.exe" + config
 subprocess.call(args, stdout=FNULL, stderr=FNULL, shell=True)
